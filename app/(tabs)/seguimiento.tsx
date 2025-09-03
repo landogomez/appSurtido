3a78b11be9e83897d9f30719b0d27f523e26aced
landito <ogomez.isc@gmail.com>
2025-09-03 13:17:22 -0600

    Basic nav

---
+++ app/(tabs)/seguimiento.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native"
+
+
+const Seguimiento = () => {
+    return (
+        <View>
+            <Text>Seguimiento</Text>
+        </View>
+    )
+}
+
+export default Seguimiento
---
+++ app/(tabs)/ventas.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native"
+
+
+const Ventas = () => {
+    return (
+        <View>
+            <Text>Ventas</Text>
+        </View>
+    )
+}
+
+export default Ventas
