5c485d3320b06295899e9cc631447ae2304ae5cf
landito <ogomez.isc@gmail.com>
2025-09-04 21:59:43 -0600

    Nav bar hecha

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,40 @@
 
 import { Tabs } from "expo-router";
-import { ClipboardDocumentCheckIcon, HomeIcon as HomeIconOutline, MapPinIcon as MapPinOutline } from "react-native-heroicons/outline";
-import { ClipboardDocumentCheckIcon as ClipboardOutline, HomeIcon as HomeIconFilled, MapPinIcon as MapPinFilled } from "react-native-heroicons/solid";
+import { TouchableOpacity, View } from "react-native";
+import { ClipboardDocumentCheckIcon as ClipboardOutline, DocumentPlusIcon, HomeIcon as HomeIconOutline, MapPinIcon as MapPinOutline } from "react-native-heroicons/outline";
+import { ClipboardDocumentCheckIcon as ClipboardFilled, HomeIcon as HomeIconFilled, MapPinIcon as MapPinFilled } from "react-native-heroicons/solid";
 
 const _Layout = () => {
     return (
-        <Tabs>
+        <>
+            <Tabs
+            screenOptions={{
+                tabBarShowLabel: false,
+                tabBarIconStyle: {
+                    marginTop: 20 // mueve el icono hacia abajo
+                },
+                tabBarItemStyle: {
+                    width: '100%',
+                    height: '100%',
+                    flex: 1,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    marginHorizontal: 10
+                },
+                tabBarStyle : {
+                    borderRadius: 50,
+                    marginHorizontal:30,
+                    marginBottom:50,
+                    height:70,
+                    position: 'absolute',
+                    overflow: 'hidden',
+                    borderColor: '#E1E1E1',
+                    borderWidth: 2,
+                    borderTopWidth: 2
+                }
+            }}
+
+        >
             <Tabs.Screen
                 name="index"
                 options={{
@@ -15,8 +44,31 @@ const _Layout = () => {
                         focused ? (
                             <HomeIconFilled />
                         ) : (
-                            <HomeIconOutline />
+                            <HomeIconOutline color={'#B1B1B1'}/>
                         )
+                }}
+            />
+            <Tabs.Screen 
+                name="nuevoTicket"
+                options={{
+                    title: 'Nuevo Ticket',
+                    headerShown: false,
+                    tabBarIcon: ({ color }) => <DocumentPlusIcon color={color || '#B1B1B1'} />,
+                }}
+            />
+            <Tabs.Screen
+                name="spacer"
+                options={{
+                tabBarButton: () => null, // no muestra nada
+                tabBarItemStyle: { flex: 1.5 }, // <-- aquí el truco
+                }}
+            />
+            <Tabs.Screen 
+                name="nuevo"
+                options={{
+                    title: 'Nuevo Ticket',
+                    headerShown: false,
+                    tabBarIcon: ({ color }) => <DocumentPlusIcon color={color || '#B1B1B1'} />,
                 }}
             />
             <Tabs.Screen
@@ -28,7 +80,7 @@ const _Layout = () => {
                         focused ? (
                             <MapPinFilled />
                         ) : (
-                            <MapPinOutline />
+                            <MapPinOutline color={'#B1B1B1'}/>
                         )
                 }}
             />
@@ -39,13 +91,47 @@ const _Layout = () => {
                     headerShown: false,
                     tabBarIcon: ({ focused }) =>
                         focused ? (
-                            <ClipboardDocumentCheckIcon />
+                            <ClipboardFilled />
                         ) : (
-                            <ClipboardOutline />
+                            <ClipboardOutline color={'#B1B1B1'}/>
                         )
                 }}
             />
         </Tabs>
+
+        <View
+            style={{
+                position: 'absolute',
+                bottom: 90,
+                left:0,
+                right: 0,
+                alignItems: 'center',
+                zIndex: 10,
+            }}
+            pointerEvents="none"
+        >
+            <TouchableOpacity
+                style={{
+                    width: 70,
+                    height: 70,
+                    borderRadius: 35,
+                    backgroundColor: '#060F28',
+                    shadowColor: '#000',
+                    shadowOffset: { width: 0, height: 4 },
+                    shadowOpacity: 0.3,
+                    shadowRadius: 8,
+                    elevation: 8,
+                }}
+                onPress={() => {
+                    // A
+                }}
+            >
+                <DocumentPlusIcon color="white" style={{ alignSelf: 'center', marginTop: 20 }} size={32}/>
+            </TouchableOpacity>
+        </View>
+    </>
+        
+        
     )
 }
 
---
+++ app/(tabs)/nuevoTicket.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+
+
+const NuevoTicket = () => {
+    return (
+        <View>
+            <Text>Nueva Orden</Text>
+        </View>
+    )
+}
+export default NuevoTicket
