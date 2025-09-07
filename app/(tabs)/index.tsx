6c30ff48e3ecdaa13fe5341e145baff5710fbac8
landito <ogomez.isc@gmail.com>
2025-09-07 13:47:47 -0600

    Prototipo del main

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import NavBar from "../navBar";
 
 const Index = () => {
@@ -8,7 +8,7 @@ const Index = () => {
                 <Text className="text-2xl text-gray-500 text-center">Ángel</Text>
                 <Text className="text-4xl  text-['#1D3557'] text-center">¿Listo para surtir hoy?</Text>
             </View>
-                <View className="w-96 h-72 self-center mt-10 flex flex-wrap">
+            <View className="w-96 h-72 self-center mt-8 flex flex-wrap">
                 {[...Array(4)].map((_, i) => (
                     <View
                         key={i}
@@ -22,7 +22,47 @@ const Index = () => {
                     </View>
                 ))}
             </View>
-            {/* ...aquí puedes agregar más contenido si lo necesitas... */}
+            <View className="relative h-68 flex flex-col px-6 pt-8 items-center justify-center mt-0">
+                <View className="w-96 flex-row justify-between mb-4">
+                    <Text className="font-bold text-gray-600">Tickets recientes</Text>
+                    <Text className="font-bold ">Hoy</Text>
+                </View>
+                <View>
+                    <View className="h-16 w-96 rounded-lg border border-gray-200 mb-2 p-4 justify-center">
+                        <View className="flex-row justify-between">
+                            <Text className="font-bold">La Moderna</Text>
+                            <Text className="font-bold">$2,000</Text>
+                        </View>
+                        <View className="flex-row justify-between">
+                            <Text className="text-gray-500">Hoy a las 10:00 AM</Text>
+                            <Text className="text-gray-500">250 Productos</Text>
+                        </View>
+                    </View>
+                    <View className="h-16 w-96 rounded-lg border border-gray-200 mb-2 p-4 justify-center">
+                        <View className="flex-row justify-between">
+                            <Text className="font-bold">La Moderna</Text>
+                            <Text className="font-bold">$2,000</Text>
+                        </View>
+                        <View className="flex-row justify-between">
+                            <Text className="text-gray-500">Hoy a las 10:00 AM</Text>
+                            <Text className="text-gray-500">250 Productos</Text>
+                        </View>
+                    </View>
+                    <View className="h-16 w-96 rounded-lg border border-gray-200 mb-2 p-4 justify-center">
+                        <View className="flex-row justify-between">
+                            <Text className="font-bold">La Moderna</Text>
+                            <Text className="font-bold">$2,000</Text>
+                        </View>
+                        <View className="flex-row justify-between">
+                            <Text className="text-gray-500">Hoy a las 10:00 AM</Text>
+                            <Text className="text-gray-500">250 Productos</Text>
+                        </View>
+                    </View>
+                </View>
+                <TouchableOpacity className="absolute bottom-1 left-0 right-0 items-center" onPress={()=>{}}>
+                    <Text className="font-bold text-blue-500">Ver todos</Text>
+                </TouchableOpacity>
+            </View>
             <NavBar />
         </View>
     )
