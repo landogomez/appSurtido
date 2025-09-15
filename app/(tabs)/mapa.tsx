6048acda646e45a10ad11748bde2bbd80e2f6c47
landito <ogomez.isc@gmail.com>
2025-09-14 19:25:55 -0600

    ya hay un mapa

--- app/(tabs)/mapa.tsx
+++ app/(tabs)/mapa.tsx
@@ -1,9 +1,19 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import MapView from 'react-native-maps';
 import NavBar from "../navBar";
+
+const INITIAL_REGION = {
+    latitude: 19.4326,
+    longitude: -99.1332,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+}
+
+
 const Mapa = () => {
     return (
-        <View className="flex-1 bg-white">
-            <Text className="text-black justify-center items-center">Mapa</Text>
+        <View className="flex-1">
+            <MapView initialRegion={INITIAL_REGION} style={{ flex: 1 }} showsUserLocation/>
             <NavBar />
         </View>
     )
